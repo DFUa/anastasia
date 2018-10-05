0b28f0eddeb46f727db8cf7b0807de569d14ae42
DFUa <dmitrosichenko@ukr.net>
2018-10-05 07:57:57 +0300

    news-list + news-item

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -119,6 +119,93 @@ $(document).ready(function() {
   $('.cancel').on('click', function() {
     $('.search-block').removeClass('show');
     $('.search-block').find('input').val('')
-  })
+  });
+
+  let newsPost = [
+    {
+      image: 'https://fakeimg.pl/380x380/?text=380x380"',
+      date: '15 серпня 2018',
+      name: 'school fest',
+      link: '#'
+    },
+    {
+      image: 'https://fakeimg.pl/500x380/?text=500x380"',
+      date: '10 серпня 2018',
+      name: 'перезавантажся разом з нами і виграй електротранспорт',
+      link: '#'
+    },
+    {
+      image: 'https://fakeimg.pl/400x200/?text=400x200"',
+      date: '20 серпня 2018',
+      name: 'серпень починається з дуже приємної новини! у etam нова колекція - осінь 2018. саме час завітати в etam.',
+      link: '#'
+    },
+    {
+      image: 'https://fakeimg.pl/400x300/?text=400x300"',
+      date: '13 серпня 2018',
+      name: 'мода на щастя! нова колекція luckylook прикрасить це літо',
+      link: '#'
+    },
+    {
+      image: 'https://fakeimg.pl/400x350/?text=400x350"',
+      date: '19 липня 2018',
+      name: 'дрім таун юквест',
+      link: '#'
+    },
+    {
+      image: 'https://fakeimg.pl/285x300/?text=285x300"',
+      date: '18 липня 2018',
+      name: 'день поцілунків в трц dream town!',
+      link: '#'
+    },
+    {
+      image: 'https://fakeimg.pl/235x300/?text=235x300"',
+      date: '17 липня 2018',
+      name: 'Here are some examples you can look at',
+      link: '#'
+    },
+    {
+      image: 'https://fakeimg.pl/295x300/?text=295x300"',
+      date: '17 липня 2018',
+      name: 'This is a first draft, so you may expect some new features soon.',
+      link: '#'
+    },
+    {
+      image: 'https://fakeimg.pl/295x300/?text=295x300"',
+      date: '20 серпня 2018',
+      name: 'Use them directly in an image tag or a CSS background',
+      link: '#'
+    }/*,
+    {
+      image: 'https://fakeimg.pl/380x300/?text=380x300"',
+      date: '21 серпня 2018',
+      name: 'Choose the size, the colors, even the text. It’s free and open-source.'
+    }*/
+  ],
+  size = newsPost.length;;
+
+  if($('.news-container').length){
+  	function item(image,date,name,link) {
+  		return `
+  				<div class="news-col-item">
+            <div class="news-col-img">
+              <img src="`+image+`" alt="#">
+            </div>
+            <p class="news-col-date">`+date+`</p>
+            <a href="`+link+`" class="news-col-name">`+name+`</a>
+            <a class="primary-link" href="`+link+`">детальніше</a>
+          </div>
+  		`
+  	}
+  	for (let i = 0; i < size; i+=3) {
+  		$('.news-col-1').append(item(newsPost[i].image, newsPost[i].date, newsPost[i].name));
+  	};
+  	for (let i = 1; i < size; i+=3) {
+  		$('.news-col-2').append(item(newsPost[i].image, newsPost[i].date, newsPost[i].name));
+  	};
+  	for (let i = 2; i < size; i+=3) {
+  		$('.news-col-3').append(item(newsPost[i].image, newsPost[i].date, newsPost[i].name));
+  	}
+  }
 
 });
